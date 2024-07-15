fe319a1326ad3bfe03a5b0a7a1e6dfebd3d8df89
seth <nguyenduong8bc1@gmail.com>
2024-07-15 17:49:14 +0930

    doing login page

--- routes/login.js
+++ routes/login.js
@@ -4,9 +4,18 @@ var path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var loginPath = __dirname + '/../views/login.html';
-    console.log('file path: ' + loginPath);
-  res.sendFile('/app/views/login.html');
+  res.render('/app/views/login.pug');
 });
+
+router.post('/' , function(req,res,next) {
+  const sampleJson = {
+    "username": "sampleName",
+    "userId": 1
+  }
+
+  console.log(req.body[1].username);
+  res.send(req.body[0].username);
+});
+
 
 module.exports = router;
